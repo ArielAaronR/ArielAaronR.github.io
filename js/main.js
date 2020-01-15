1e87231469ebcca2df25adde2f394c6cfe4dad1f
Aaron Resurreccion <aaronresurreccion@gmail.com>
2020-01-14 16:16:39 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// window.onbeforeunload = function() {
+//   if (window.scrollTo) window.scrollTo(0, 0);
+// };
+
+ScrollReveal().reveal(".about", {
+  delay: 600,
+  easing: "cubic-bezier(.42, 0, .58, 1)",
+  reset: false
+});
+ScrollReveal().reveal(".projects", {
+  delay: 700,
+  easing: "cubic-bezier(.42, 0, .58, 1)",
+  reset: false
+});
+ScrollReveal().reveal(".skills", {
+  delay: 800,
+  reset: false,
+  easing: "cubic-bezier(.42, 0, .58, 1)",
+
+});
+ScrollReveal().reveal(".resume", {
+  delay: 900,
+  easing: "cubic-bezier(.42, 0, .58, 1)",
+  reset: false
+});
+
+
