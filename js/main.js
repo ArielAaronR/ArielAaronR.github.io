df72c781cb7b67810e624409328d32500f7c475d
Aaron Resurreccion <aaronresurreccion@gmail.com>
2020-01-20 09:53:14 -0800

    changed delay amount on scroll

--- js/main.js
+++ js/main.js
@@ -3,27 +3,27 @@
 // };
 
 ScrollReveal().reveal(".about", {
-  delay: 600,
+  delay: 100,
   easing: "cubic-bezier(.42, 0, .58, 1)",
   reset: false,
   mobile: true
 });
 ScrollReveal().reveal(".skills", {
-  delay: 700,
+  delay: 125,
   reset: false,
   mobile: true,
   easing: "cubic-bezier(.42, 0, .58, 1)"
 });
 
 ScrollReveal().reveal(".resume", {
-  delay: 800,
+  delay: 150,
   easing: "cubic-bezier(.42, 0, .58, 1)",
   reset: false,
   mobile: true
 });
 
 ScrollReveal().reveal(".projects", {
-  delay: 900,
+  delay: 175,
   easing: "cubic-bezier(.42, 0, .58, 1)",
   reset: false,
   mobile: true
